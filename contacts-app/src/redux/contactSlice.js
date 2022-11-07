534b098f3e31437295a80638a3bd14f20397526f
Ezgi Hocaoğlu <ezgihocaoglu@hotmail.com>
2022-11-07 15:19:21 +0300

    add redux

---
+++ contacts-app/src/redux/contactSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactSlice = createSlice({
+    name: 'contacts',
+    initialState: [],
+    reducers: {
+    }
+})
+
+export default contactSlice.reducer
